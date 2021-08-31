aadfc9f402eceb1d1d6758fe2d0b24cbcc335901
[fanian] <[rfanian@gmail.com]>
2021-08-31 12:22:19 +0200

    beginning stage 6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
